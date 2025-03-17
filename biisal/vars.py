be65a946540d7125aebd4c91b2d797de56d18d4f
chinnukush <chinnukushal123@gmail.com>
2025-03-17 23:05:12 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -18,13 +18,13 @@ class Var(object):
     name = str(getenv('name', 'HK_Streamx_Bit'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', ''))
-    NEW_USER_LOG = int(getenv('NEW_USER_LOG', ''))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002210651858'))
+    NEW_USER_LOG = int(getenv('NEW_USER_LOG', '-1002170171844'))
     PORT = int(getenv('PORT', '8080'))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = [int(x) for x in os.environ.get("OWNER_ID", "5397984467").split()]
-    NO_PORT = bool(getenv('NO_PORT', False))
+    NO_PORT = bool(getenv('NO_PORT', True)
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Harikushal'))
     if 'DYNO' in environ:
@@ -33,13 +33,13 @@ class Var(object):
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', '') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT')) if not ON_HEROKU or getenv('FQDN', 'appropriate-corry-koyebuser123-031b46d3.koyeb.app/') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',True))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ''))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://anikush8310:AmEPHgfyBCgjGcAF@cluster0.tsu0s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'TG_BOTS_UPDATE')) 
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))   
     BAN_CHNL = list(set(int(x) for x in str(getenv("BAN_CHNL", "")).split()))   
